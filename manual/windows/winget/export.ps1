5d93cdfbb724035656d62cac75bc32b05a96ebb7
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-06-04 11:45:42 +0900

    modify winget

--- manual/windows/winget/export.ps1
+++ manual/windows/winget/export.ps1
@@ -2,4 +2,4 @@ $configRelativePath = "manual\windows\winget\packages.json"
 $gitRoot = Invoke-Expression "git rev-parse --show-toplevel"
 $configPath = Join-Path $gitRoot $configRelativePath
 
-winget export --config $configPath --disable-interactivity --accept-source-agreements --verbose
+winget export $configPath --disable-interactivity --accept-source-agreements --verbose
