e6f72ad0fef390e5f7e43e2de1abc5030994abcb
TakashiAihara <47384567+TakashiAihara@users.noreply.github.com>
2023-12-17 10:40:32 +0000

    migrate windows packages to wingetui

--- manual/windows/winget/export.ps1
+++
@@ -1,5 +0,0 @@
-$configRelativePath = "manual\windows\winget\packages.json"
-$gitRoot = Invoke-Expression "git rev-parse --show-toplevel"
-$configPath = Join-Path $gitRoot $configRelativePath
-
-winget export $configPath --disable-interactivity --accept-source-agreements --verbose
